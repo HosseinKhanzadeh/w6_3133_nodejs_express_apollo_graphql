7f5cbc9ca4255bc69cb78cc2f65742f148af4801
MrKhan <152137159+srho3in@users.noreply.github.com>
2026-02-22 01:39:52 -0500

    Commit 1: Defined GraphQL Movie schema with required queries and mutations

--- resolvers/resolvers.js
+++ resolvers/resolvers.js
@@ -1,3 +1,18 @@
 import MovieModel from '../models/movie.js';
 
 // Resolvers define the technique for fetching the types defined in the schema.
+
+const resolvers = {
+  Query: {
+    movies: () => [],
+    movie: () => null,
+    moviesByDirector: () => [],
+  },
+  Mutation: {
+    addMovie: () => null,
+    updateMovie: () => null,
+    deleteMovie: () => '',
+  },
+};
+
+export default resolvers;
